087f5464e3b492b38a637bb4feed3bcb7e3891fa
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-21 20:06:42 +0300

    add admin area

---
+++ MediTrack/Areas/Admin/Controllers/AdminAuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediTrack.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("api/[area]/[contoller]")]
+    [ApiController]
+
+    public class AdminAuthController : Controller
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "This is Admin Area" });
+        }
+    }
+}
