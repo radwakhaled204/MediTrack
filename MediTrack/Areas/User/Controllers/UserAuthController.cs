6c2c2b2b5a1d72ac292975bbdc220ab7aa18ba23
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-22 14:47:44 +0300

    Add User Controller to test User Area

---
+++ MediTrack/Areas/User/Controllers/UserAuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediTrack.Areas.User.Controllers
+{
+    [Area("User")]
+    [Route("api/[area]/[Controller]")]
+    [ApiController]
+   
+    public class UserAuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {Message="This is User Area"});
+        }
+    }
+}
