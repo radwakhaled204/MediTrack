f2bd6c25dbadbf31ba42396d081a5ceef66779ef
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-22 17:18:05 +0300

    add user table in datacontext

--- MediTrack/Areas/User/Data/DataContext.cs
+++ MediTrack/Areas/User/Data/DataContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using MediTrack.Areas.User.Models;
 namespace MediTrack.Areas.User.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<MediTrack.Areas.User.Models.User> users { get; set; }
     }
+   
 }
