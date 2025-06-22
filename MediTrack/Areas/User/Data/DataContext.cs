c3e1111b807fcedc878be28ec12c044f92df7029
Radwa Khaled <radwaabouzed@gmail.com>
2025-06-22 15:15:45 +0300

    Add DataContext to User Area

---
+++ MediTrack/Areas/User/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MediTrack.Areas.User.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
