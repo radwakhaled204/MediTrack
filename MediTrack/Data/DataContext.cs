a0a8b36b0f547537d3fd71d918ad8caca11064ec
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-09 16:58:49 +0300

    add  DataContext to project

---
+++ MediTrack/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MediTrack.Models;
+namespace MediTrack.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> users { get; set; }
+    }
+
+}
