7fbfa383f7fe7efda59c200d8e0e78a370fd4fec
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-09 16:59:03 +0300

    add  DataRepository to project

---
+++ MediTrack/Data/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MediTrack.Data
+{
+    public class DataRepository<T>:IDataRepository<T> where T : class
+    {
+        private readonly DataContext _db;
+        private readonly DbSet<T> table;
+        public DataRepository(DataContext db)
+        {
+            _db = db;
+            table = _db.Set<T>();
+        }
+    }
+   
+}
