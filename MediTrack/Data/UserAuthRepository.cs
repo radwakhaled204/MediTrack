7d96ecb6956e9573d7e083cf5b29fc85c9405a63
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-09 19:04:07 +0300

    add DataContext to UserAuthRepository

--- MediTrack/Data/UserAuthRepository.cs
+++ MediTrack/Data/UserAuthRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace MediTrack.Data
+﻿using MediTrack.Models;
+namespace MediTrack.Data
 {
-    public class UserAuthRepository
+    public class UserAuthRepository : IUserAuthRepository
     {
+        private readonly DataContext _db;
+        public UserAuthRepository(DataContext db)
+        {
+            _db = db;
+        }
     }
 }
