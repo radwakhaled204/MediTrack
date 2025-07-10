3194af3be7fcc1114d3793fe343e7b62e469ce7e
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-10 16:25:47 +0300

    Add Jwt Configurations

---
+++ MediTrack/Configurations/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MediTrack.Configurations
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+        public string Issure {  get; set; }
+        public string Audience { get; set; }
+    }
+}
