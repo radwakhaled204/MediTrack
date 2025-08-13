2865aaa9228634390d53ba1c3ec0eadd45b23d87
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-13 15:42:27 +0300

    Implement the basee functions

--- MediTrack/Data/DataRepository.cs
+++ MediTrack/Data/DataRepository.cs
@@ -11,6 +11,20 @@ namespace MediTrack.Data
             _db = db;
             table = _db.Set<T>();
         }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           return  await table.ToListAsync();
+        }
+        public async Task<T> GetById(int? id)
+        {
+           var result = await table.FindAsync(id);
+            if (result == null)
+            {
+                throw new KeyNotFoundException($"Cannot Found This Id {id}");
+            }
+                return result;
+        }
+
     }
    
 }
