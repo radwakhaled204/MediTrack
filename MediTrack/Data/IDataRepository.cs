992c467451eb6c1cdb0b73d66ccf6c0cb4a6d7cd
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-13 15:42:42 +0300

    Add the base Functions

--- MediTrack/Data/IDataRepository.cs
+++ MediTrack/Data/IDataRepository.cs
@@ -2,6 +2,11 @@
 {
     public interface IDataRepository<T> where T : class
     {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetById(int? id);
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(int? id);
 
     }
 }
