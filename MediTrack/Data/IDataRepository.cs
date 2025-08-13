ec3e99b05da499ac6e55e8e683b5feb1b4b4a6d3
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-13 15:54:02 +0300

    Modify functions

--- MediTrack/Data/IDataRepository.cs
+++ MediTrack/Data/IDataRepository.cs
@@ -2,11 +2,11 @@
 {
     public interface IDataRepository<T> where T : class
     {
-        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAll();
         Task<T> GetById(int? id);
-        Task<T> Add(T entity);
-        Task<T> Update(T entity);
-        Task<T> Delete(int? id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Delete(int? id);
 
     }
 }
