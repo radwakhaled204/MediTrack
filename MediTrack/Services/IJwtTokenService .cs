bfe738fdddaacec4f7196cfcf41caf2fcd6b7758
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-14 18:21:13 +0300

    add IJwtTokenService

---
+++ MediTrack/Services/IJwtTokenService .cs
@@ -0,0 +1,10 @@
+﻿
+using System.Security.Claims;
+namespace MediTrack.Services
+{
+    public interface IJwtTokenService
+    {
+        public string GenerateJWT(IEnumerable<Claim> claims);
+
+    }
+}
