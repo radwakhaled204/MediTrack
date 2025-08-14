a845a538bd099855c8eceba5a3003da7352f1a11
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-14 18:34:06 +0300

    add logindto

---
+++ MediTrack/Dtos/LoginDtos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediTrack.Dtos
+{
+    public class LoginDtos
+    {
+        [EmailAddress(ErrorMessage = "Please Enter A Valid Email")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string password { get; set; }
+    }
+}
