b55bb2ee8136945c3ea7f4e17beab8c8579dfc85
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-14 18:34:15 +0300

    add userdto

---
+++ MediTrack/Dtos/UserDtos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediTrack.Dtos
+{
+    public class UserDtos
+    {
+        [Required]
+        public string user_name { get; set; }
+
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+
+    }
+}
