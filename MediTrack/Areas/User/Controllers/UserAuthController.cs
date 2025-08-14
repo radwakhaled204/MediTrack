30dfd26bd616b99420ee6364dec57f87a4f1f872
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-14 19:21:22 +0300

    add DI to controller

--- MediTrack/Areas/User/Controllers/UserAuthController.cs
+++ MediTrack/Areas/User/Controllers/UserAuthController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.EntityFrameworkCore;
+using MediTrack.Data;
+using MediTrack.Dtos;
+using MediTrack.Models;
+using MediTrack.Services;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MediTrack.Areas.User.Controllers
 {
@@ -6,12 +12,22 @@ namespace MediTrack.Areas.User.Controllers
     [Route("api/[area]/[Controller]")]
     [ApiController]
    
-    public class UserAuthController : Controller
+    public class UserAuthController : ControllerBase
     {
+        private readonly IUserAuthRepository _userAuthRepository;
+        private readonly IDataRepository<MediTrack.Models.User> _dataRepository;
+        public UserAuthController(IUserAuthRepository userAuthRepository , IDataRepository<MediTrack.Models.User> dataRepository)
+        {
+            _userAuthRepository = userAuthRepository;
+            _dataRepository = dataRepository;
+        }
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(new { message= "this user area" });
         }
+
+        
+        
     }
 }
