fe583b5937d163f71b4cb62151a842710c0770a7
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-15 16:00:43 +0300

    up

---
+++ MediTrack/Dtos/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediTrack.Dtos
+{
+    public class RegisterDto
+    {
+        [StringLength(20, MinimumLength = 3 ,ErrorMessage = "Enter Name Between 3 And 20 Character")]
+        public string user_name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please Enter A Valid Email")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string password { get; set; }
+    }
+}
