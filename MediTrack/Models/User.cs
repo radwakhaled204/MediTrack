101718daa42031f531fcc2bc8f26f96b7bac5a41
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-15 17:49:33 +0300

    update user model

--- MediTrack/Models/User.cs
+++ MediTrack/Models/User.cs
@@ -8,14 +8,13 @@ namespace MediTrack.Models
         public int user_id { get; set; }
         [Required]
         public string user_name { get; set; }
-        [Required]
-        public string password { get; set; }
+
         public byte[] password_hash { get; set; }
         public byte[] password_salt { get; set; }
         [Required]
         public string email { get; set; }
-        public string user_code { get; set; }
-        public int phone_num { get; set; }
+        public string? user_code { get; set; }
+        public int? phone_num { get; set; }
         public DateTime JoinedDate { get; set; }
 
 
