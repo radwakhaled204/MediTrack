3cefdb63b10c60d463267c60ca1d799d06297431
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-18 15:24:13 +0300

    up

--- MediTrack/Dtos/ChangePasswordDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MediTrack.Dtos
-{
-    public class ChangePasswordDto
-    {
-       public int user_id { get; set; }
-       public string old_password { get; set; } = string.Empty;
-       public string new_password { get; set; } = string.Empty;
-    }
-}
