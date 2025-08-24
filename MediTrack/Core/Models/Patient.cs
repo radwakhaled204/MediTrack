174814401e8717d800680caf1a1045cbb844d330
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-24 19:37:23 +0300

    +patient table

---
+++ MediTrack/Core/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MediTrack.Core.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int patient_id { get; set; }
+        public string full_name { get; set; }
+        public DateTime birth_date { get; set; }
+        public string gender { get; set; }
+        public string? patient_code { get; set; }
+        public int? phone_num { get; set; }
+        public string? address { get; set; }
+        public User user_id { get; set; }
+        public Insurance insurance_id { get; set; }
+    }
+}
