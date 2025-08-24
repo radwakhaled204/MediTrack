6ceef450808d723da3b92e8a34fa76eef1b60a1b
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-24 19:37:11 +0300

    +insurance table

---
+++ MediTrack/Core/Models/Insurance.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MediTrack.Core.Models
+{
+    public class Insurance
+    {
+        [Key]
+        public int insurance_id { get; set; }
+        public string provider_name { get; set; }
+        public string coverage_details { get; set; }
+        public int policy_num { get; set; }
+        public DateTime valid_from { get; set; }
+        public DateTime valid_to { get; set; }
+
+    }
+}
