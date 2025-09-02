42a87e654deb614839f872d838ffff728c9d8f55
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-02 12:30:13 +0300

    up

---
+++ MediTrack/Migrations/20250902085740_again.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediTrack.Migrations
+{
+    /// <inheritdoc />
+    public partial class again : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "insurances",
+                columns: table => new
+                {
+                    InsuranceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProviderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CoverageDetails = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PolicyNum = table.Column<int>(type: "int", nullable: false),
+                    ValidFrom = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValidTo = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_insurances", x => x.InsuranceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JoinedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "patients",
+                columns: table => new
+                {
+                    PatientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PatientCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNum = table.Column<int>(type: "int", nullable: true),
+                    NationalId = table.Column<int>(type: "int", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    InsuranceId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_patients", x => x.PatientId);
+                    table.ForeignKey(
+                        name: "FK_patients_insurances_InsuranceId",
+                        column: x => x.InsuranceId,
+                        principalTable: "insurances",
+                        principalColumn: "InsuranceId",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_patients_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_patients_InsuranceId",
+                table: "patients",
+                column: "InsuranceId",
+                unique: true,
+                filter: "[InsuranceId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_patients_UserId",
+                table: "patients",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "patients");
+
+            migrationBuilder.DropTable(
+                name: "insurances");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
