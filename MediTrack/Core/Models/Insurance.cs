84fcbd3b68556fe0aab6f6893f073ac565d16f15
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-02 12:28:44 +0300

    modify insurance

--- MediTrack/Core/Models/Insurance.cs
+++ MediTrack/Core/Models/Insurance.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+
 namespace MediTrack.Core.Models
 {
     public class Insurance
     {
         [Key]
-        public int insurance_id { get; set; }
-        public string provider_name { get; set; }
-        public string coverage_details { get; set; }
-        public int policy_num { get; set; }
-        public DateTime valid_from { get; set; }
-        public DateTime valid_to { get; set; }
-
+        public int InsuranceId { get; set; }
+        public string ProviderName { get; set; }
+        public string CoverageDetails { get; set; }
+        public int PolicyNum { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
     }
 }
