4a2987241d7132541adb961b284dd55011845550
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-04 15:14:52 +0300

    add automapper

---
+++ MediTrack/WebAPI/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediTrack.Core.Models;
+using MediTrack.WebAPI.Dtos;
+
+namespace MediTrack.WebAPI.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        { 
+
+           CreateMap<Patient , PatientDto>().ReverseMap();
+           
+
+        }
+    }
+}
