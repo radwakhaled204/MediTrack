67651857bd13cd9d25e6081998c6a408498710d4
Radwa Khaled <radwaabouzed@gmail.com>
2025-09-04 15:15:53 +0300

    add patient controller

---
+++ MediTrack/WebAPI/Areas/User/Controllers/PatientsController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediTrack.Core.Interfaces;
+using MediTrack.Core.Models;
+using MediTrack.WebAPI.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediTrack.WebAPI.Areas.User.Controllers
+{
+    [Area("User")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IDataRepository<Patient> _dataRepository;
+        private readonly IMapper _mapper;
+
+        public PatientsController(IDataRepository<Patient> dataRepository, IMapper mapper)
+        {
+            _dataRepository = dataRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePatient([FromBody] PatientDto patientDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var patient = _mapper.Map<Patient>(patientDto);
+            await _dataRepository.Add(patient);
+
+            // ممكن ترجعي الـ patient اللي اتحفظ أو DTO بعد المابنج
+            return Ok(_mapper.Map<PatientDto>(patient));
+        }
+    }
+}
